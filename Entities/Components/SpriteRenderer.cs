9c4926e3939851bc8afccca30d439beac881edde
Vorlias <void.knight09@gmail.com>
2017-05-24 02:52:42 +1200

    Added SpriteRenderer component

---
+++ Entities/Components/SpriteRenderer.cs
@@ -0,0 +1,25 @@
+﻿using SFML.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VorliasEngine2D.Entities.Components
+{
+    class SpriteRenderer : Drawable, IComponent
+    {
+        public string Name
+        {
+            get
+            {
+                return "SpriteRenderer";
+            }
+        }
+
+        public void Draw(RenderTarget target, RenderStates states)
+        {
+            // TODO: Add sprite stuff here
+        }
+    }
+}
