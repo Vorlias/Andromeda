e328c54b3082cdbdae66246c39167a934e1c711a
Vorlias <void.knight09@gmail.com>
2017-05-24 02:44:40 +1200

    Added component interface

---
+++ Entities/Components/IComponent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VorliasEngine2D.Entities.Components
+{
+    public interface IComponent
+    {
+        string Name
+        {
+            get;
+        }
+    }
+}
