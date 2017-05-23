8c1527060c980539a8026094ca5941a94d782e77
Vorlias <void.knight09@gmail.com>
2017-05-24 02:44:23 +1200

    Added Entity + Components

---
+++ Entities/Components/Transform.cs
@@ -0,0 +1,20 @@
+﻿using SFML.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VorliasEngine2D.Entities.Components
+{
+    class Transform : Transformable, IComponent
+    {
+        public string Name
+        {
+            get
+            {
+                return "Transform";
+            }
+        }
+    }
+}
---
+++ Entities/Entity.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VorliasEngine2D.Entities.Components;
+
+namespace VorliasEngine2D.Entities
+{
+    class Entity
+    {
+        HashSet<IComponent> components;
+        Transform transform;
+
+        /// <summary>
+        /// The Transform of this entity
+        /// </summary>
+        public Transform Transform
+        {
+            get
+            {
+                if (transform == null)
+                    transform = AddComponent<Transform>();
+
+                return transform;
+            }
+        }
+
+        public T AddComponent<T>() where T : IComponent, new()
+        {
+            T component = new T();
+            T existing = (T) components.First(c => component.Name == c.Name);
+
+            if (existing != null)
+            {
+                return existing;
+            }
+            else
+            {
+                components.Add(component);
+                return component;
+            }
+        }
+
+        public Entity()
+        {
+            
+        }
+    }
+}
