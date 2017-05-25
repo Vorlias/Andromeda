79acc06deedec8a0d2e078106ce83871ef618abe
Vorlias <void.knight09@gmail.com>
2017-05-25 14:58:24 +1200

    Added IInstanceTree interface

---
+++ System/IInstanceTree.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VorliasEngine2D.Entities;
+
+namespace VorliasEngine2D.System
+{
+    /// <summary>
+    /// Interface for instance containers
+    /// </summary>
+    public interface IInstanceTree
+    {
+        Entity FindFirstChild(string name);
+        Entity[] GetChildren();
+    }
+}
