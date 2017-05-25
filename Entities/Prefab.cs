2f0553ed2e4c2da3a06ce406c3bb07e07183a8b4
Vorlias <void.knight09@gmail.com>
2017-05-25 16:34:42 +1200

    Added Prefab class for entity cloning from original

---
+++ Entities/Prefab.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VorliasEngine2D.Entities
+{
+    /// <summary>
+    /// A class that handles preset entities
+    /// </summary>
+    public sealed class Prefab
+    {
+        Entity original;
+        Entity[] entities;
+
+        private Prefab()
+        {
+
+        }
+
+        /// <summary>
+        /// Creates a copy of the prefab entity
+        /// </summary>
+        /// <returns>A copy of the prefab entity</returns>
+        public Entity Clone()
+        {
+            return original.Clone();
+        }
+
+        public Prefab Create(Entity original)
+        {
+            Prefab prefab = new Prefab();
+            prefab.original = original;
+
+            return prefab;
+        }
+    }
+}
