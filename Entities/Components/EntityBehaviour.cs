4f743c212433cad90764cb2d56820c13852bb7cd
Vorlias <void.knight09@gmail.com>
2017-05-25 12:21:17 +1200

    Added EntityBehaviour component
    Works similar to MonoBehaviour in the Unity engine

---
+++ Entities/Components/EntityBehaviour.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VorliasEngine2D.Entities.Components
+{
+    /// <summary>
+    /// Base class for custom scripted components
+    /// </summary>
+    class EntityBehaviour : IComponent
+    {
+        private Entity entity;
+
+        public SpriteRenderer SpriteRenderer
+        {
+            get
+            {
+                return entity.SpriteRenderer;
+            }
+        }
+
+        public Transform Transform
+        {
+            get
+            {
+                return entity.Transform;
+            }
+        }
+
+        public Entity Entity
+        {
+            get
+            {
+                return entity;
+            }
+
+            set
+            {
+                entity = value;
+            }
+        }
+
+        public virtual void Init()
+        {
+
+        }
+
+        public virtual void Awake()
+        {
+
+        }
+
+        public virtual void Update()
+        {
+
+        }
+
+        public virtual void Render()
+        {
+
+        }
+
+        public virtual void BeforeUpdate()
+        {
+
+        }
+
+        public virtual void AfterUpdate()
+        {
+
+        }
+
+        public string Name
+        {
+            get
+            {
+                return "EntityBehaviour";
+            }
+        }
+    }
+}
