f89cdd385cb03dfed516e847b3f4a90e2af9871e
Vorlias <void.knight09@gmail.com>
2017-05-28 16:09:05 +1200

    Added SpriteRenderer rotation

--- System/Utility/VectorUtil.cs
+++ System/Utility/VectorUtil.cs
@@ -7,6 +7,36 @@ using System.Threading.Tasks;
 
 namespace VorliasEngine2D.System.Utility
 {
+    public static class VectorExtension
+    {
+        /// <summary>
+        /// Gets the angle between this vector and another vector
+        /// </summary>
+        /// <param name="left">The source vector</param>
+        /// <param name="right">The target vector</param>
+        /// <returns>The angle between the two vectors</returns>
+        public static float GetAngle(this Vector2f left, Vector2f right)
+        {
+            return (float) VectorUtil.GetAngle(left, right);
+        }
+
+        /// <summary>
+        /// Returns the normal of the vector
+        /// </summary>
+        /// <param name="self"></param>
+        /// <returns>The normal of this vector</returns>
+        public static Vector2f Normalize(this Vector2f self)
+        {
+            float length = (float) Math.Sqrt(self.X * self.X + self.Y * self.Y);
+            return new Vector2f(self.X / length, self.Y / length);
+        }
+
+        public static Vector2f Rotate(this Vector2f self, float angle)
+        {
+            return VectorUtil.GetRotatedVector(self, angle);
+        }
+    }
+
     internal class VectorUtil
     {
         const double DEGREES_TO_RADIANS = Math.PI / 180;
@@ -27,6 +57,12 @@ namespace VorliasEngine2D.System.Utility
                 targetCos * vector.X - targetSin * vector.Y,
                 targetSin * vector.X - targetCos * vector.Y
             );
+        }
+
+        public static double GetAngle(Vector2f end, Vector2f start)
+        {
+            Vector2f dir = (end - start);
+            return RADIANS_TO_DEGREES * Math.Atan2(-dir.X, dir.Y);
         }
     }
 }
