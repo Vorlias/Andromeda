1b8d2a18c73d499050bac7e45a69778e38c5b293
Vorlias <void.knight09@gmail.com>
2017-05-28 16:47:47 +1200

    Added Exception for invalid component pairs

--- Entities/Components/IComponent.cs
+++ Entities/Components/IComponent.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace VorliasEngine2D.Entities.Components
 {
-    class SetEntityInvalidException : Exception
+    /// <summary>
+    /// Exception thrown if two components 'clash'
+    /// </summary>
+    public class InvalidComponentPairException : Exception
+    {
+        public InvalidComponentPairException(IComponent a, IComponent b) : base ("Cannot have a " + a.Name + " component with a " + b.Name + " component!")
+        {
+
+        }
+    }
+
+    public class SetEntityInvalidException : Exception
     {
         public SetEntityInvalidException() : base("Attempted to set parent of a Component")
         {
