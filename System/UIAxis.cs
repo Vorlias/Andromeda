478aa0af2ac9e1ca9c9b63e9b030ec463a330335
Vorlias <void.knight09@gmail.com>
2017-07-01 19:52:59 +1200

    Updated UICoordinates, refactored it.

---
+++ System/UIAxis.cs
@@ -0,0 +1,39 @@
+﻿namespace VorliasEngine2D.System
+{
+    public struct UIAxis
+    {
+        public float Scale
+        {
+            get;
+            set;
+        }
+
+        public float Offset
+        {
+            get;
+            set;
+        }
+
+        public UIAxis(float scale, float offset)
+        {
+            Scale = scale;
+            Offset = offset;
+        }
+
+        public static UIAxis operator +(UIAxis left, UIAxis right)
+        {
+            return new UIAxis(left.Scale + right.Scale, left.Offset + right.Offset);
+        }
+
+        public static UIAxis operator +(UIAxis left, float right)
+        {
+            return new UIAxis(left.Scale, left.Offset + right);
+        }
+
+
+        public override string ToString()
+        {
+            return Scale + ", " + Offset;
+        }
+    }
+}
--- System/UICoordinates.cs
+++ System/UICoordinates.cs
@@ -9,43 +9,6 @@ using VorliasEngine2D.System.Utility;
 
 namespace VorliasEngine2D.System
 {
-    public struct UIAxis
-    {
-        public float Scale
-        {
-            get;
-            set;
-        }
-
-        public float Offset
-        {
-            get;
-            set;
-        }
-
-        public UIAxis(float scale, float offset)
-        {
-            Scale = scale;
-            Offset = offset;
-        }
-
-        public static UIAxis operator +(UIAxis left, UIAxis right)
-        {
-            return new UIAxis(left.Scale + right.Scale, left.Offset + right.Offset);
-        }
-
-        public static UIAxis operator +(UIAxis left, float right)
-        {
-            return new UIAxis(left.Scale, left.Offset + right);
-        }
-
-
-        public override string ToString()
-        {
-            return Scale + ", " + Offset;
-        }
-    }
-
     /// <summary>
     /// A vector that represents a UI coordinate
     /// (scale, offset) for (x, y)
