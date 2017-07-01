551e6a23723ce9fed114b14dd64a097cda7badd2
Vorlias <void.knight09@gmail.com>
2017-07-01 19:53:28 +1200

    Added ThreadedService
    This is for experimental upcoming services, such as the ThreadService
    and possibly a ClientService and ServerService (for multiplayer - ?
    :+1:)

---
+++ System/Utility/ThreadedService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace VorliasEngine2D.System.Services
+{
+    /// <summary>
+    /// A service that runs on a separate thread
+    /// </summary>
+    public abstract class ThreadedService
+    {
+        protected abstract void Run();
+
+        Thread thread;
+        public Thread Thread
+        {
+            get;
+        }
+
+        bool running = false;
+
+        /// <summary>
+        /// Whether or not the ThreadedService is running
+        /// </summary>
+        public bool IsRunning
+        {
+            get => running;
+        }
+
+        public void Start()
+        {
+            ThreadStart start = new ThreadStart(Run);
+            thread = new Thread(start);
+            thread.Start();
+            running = true;
+        }
+
+        public void Terminate()
+        {
+            running = false;
+        }
+    }
+}
