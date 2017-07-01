af12215cc45e73be398558a1ddfb0288bc63fe7e
Vorlias <void.knight09@gmail.com>
2017-07-01 17:24:14 +1200

    Moved MouseCoordinates to it's own file

--- Entities/Components/Behaviours/EntityBehaviour.cs
+++ Entities/Components/Behaviours/EntityBehaviour.cs
@@ -1,69 +1,14 @@
-﻿using SFML.System;
-using SFML.Window;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using VorliasEngine2D.Entities.Components.Internal;
 using VorliasEngine2D.System;
-using VorliasEngine2D.System.Utility;
 using SFML.Graphics;
 
 namespace VorliasEngine2D.Entities.Components
 {
-    /// <summary>
-    /// The coordinates of the mouse
-    /// </summary>
-    public struct MouseCoordinates
-    {
-        public GameView View
-        {
-            get;
-        }
-
-        /// <summary>
-        /// The position of the mouse relative to the world
-        /// </summary>
-        public Vector2f WorldPosition
-        {
-            get;
-        }
-
-        /// <summary>
-        /// The position of the mouse relative to the window
-        /// </summary>
-        public Vector2f WindowPosition
-        {
-            get;
-        }
-
-        internal MouseCoordinates(Application application, GameView view)
-        {
-            Vector2i mousePosition = Mouse.GetPosition(application.Window);
-            View = view;
-
-            if (view.Camera != null && view.Camera.View != null)
-            {
-                View cameraView = view.Camera.View;
-                Vector2f viewCenter = cameraView.Center;
-                Vector2u windowSize = application.Window.Size;
-                Camera defaultCamera = view.Camera;
-                Vector2f offset = new Vector2f(
-                    viewCenter.X - ( windowSize.X / 2 ) + mousePosition.X,
-                    viewCenter.Y - ( windowSize.Y / 2 ) + mousePosition.Y
-                );
-
-                WorldPosition = offset.Rotate(cameraView.Rotation);
-                WindowPosition = new Vector2f(mousePosition.X, mousePosition.Y);
-            }
-            else
-            {
-                WindowPosition = new Vector2f(mousePosition.X, mousePosition.Y);
-                WorldPosition = WindowPosition;
-            }
-        }
-    }
 
     /// <summary>
     /// Base class for custom scripted components
---
+++ System/Input/MouseCoordinates.cs
@@ -0,0 +1,62 @@
+﻿using SFML.System;
+using SFML.Window;
+using VorliasEngine2D.System;
+using VorliasEngine2D.System.Utility;
+using SFML.Graphics;
+using VorliasEngine2D.Entities.Components;
+
+namespace VorliasEngine2D.System
+{
+    /// <summary>
+    /// The coordinates of the mouse
+    /// </summary>
+    public struct MouseCoordinates
+    {
+        public GameView View
+        {
+            get;
+        }
+
+        /// <summary>
+        /// The position of the mouse relative to the world
+        /// </summary>
+        public Vector2f WorldPosition
+        {
+            get;
+        }
+
+        /// <summary>
+        /// The position of the mouse relative to the window
+        /// </summary>
+        public Vector2f WindowPosition
+        {
+            get;
+        }
+
+        internal MouseCoordinates(Application application, GameView view)
+        {
+            Vector2i mousePosition = Mouse.GetPosition(application.Window);
+            View = view;
+
+            if (view.Camera != null && view.Camera.View != null)
+            {
+                View cameraView = view.Camera.View;
+                Vector2f viewCenter = cameraView.Center;
+                Vector2u windowSize = application.Window.Size;
+                Camera defaultCamera = view.Camera;
+                Vector2f offset = new Vector2f(
+                    viewCenter.X - ( windowSize.X / 2 ) + mousePosition.X,
+                    viewCenter.Y - ( windowSize.Y / 2 ) + mousePosition.Y
+                );
+
+                WorldPosition = offset.Rotate(cameraView.Rotation);
+                WindowPosition = new Vector2f(mousePosition.X, mousePosition.Y);
+            }
+            else
+            {
+                WindowPosition = new Vector2f(mousePosition.X, mousePosition.Y);
+                WorldPosition = WindowPosition;
+            }
+        }
+    }
+}
