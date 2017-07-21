c4e981546b63d0aa90592dd9d2c96c57f5597518
Vorlias <void.knight09@gmail.com>
2017-07-22 00:41:21 +1200

    Added EngineInfo

---
+++ System/EngineInfo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VorliasEngine2D.System
+{
+    public static class EngineInfo
+    {
+        const int DATE = 170722;
+        const int MAJOR = 0;
+        const int MINOR = 226;
+        const int REVISION = 0;
+
+        const int FULL = (MAJOR * 10000) + (MINOR * 100) + REVISION;
+
+        public static string String
+        {
+            get
+            {
+                return string.Format("{0}.{1}.{2}", MAJOR, MINOR, REVISION);
+            }
+        }
+
+        public static string FullString
+        {
+            get
+            {
+                return string.Format("{0}.{1}.{2}.{3}", MAJOR, MINOR, REVISION, DATE);
+            }
+        }
+    }
+}
