1667665b90b46099b113c1d1fc3078367ad8b969
Vorlias <void.knight09@gmail.com>
2017-08-22 23:52:37 +1200

    Added Network to UpdatePriority

--- Entities/Components/Internal/UpdatePriority.cs
+++ Entities/Components/Internal/UpdatePriority.cs
@@ -5,36 +5,43 @@
     /// </summary>
     public enum UpdatePriority
     {
+        
+
         /// <summary>
         /// Updates before everything else
         /// </summary>
-        First = 1,
+        First = 0,
+
+        /// <summary>
+        /// Updates when the network does (if using Vorlias2D.Multiplayer)
+        /// </summary>
+        Network = 50,
 
         /// <summary>
         /// Updates when physics does
         /// </summary>
-        Physics = 5,
+        Physics = 100,
 
         /// <summary>
         /// Updates when the camera does
         /// </summary>
-        Camera = 10,
+        Camera = 150,
 
         
 
         /// <summary>
         /// Updates when the interfaces do
         /// </summary>
-        Interface = 50,
+        Interface = 200,
 
         /// <summary>
         /// Updates at normal
         /// </summary>
-        Normal = 100,
+        Normal = 250,
 
         /// <summary>
         /// Updates last, after everything else.
         /// </summary>
-        Last = 1000,
+        Last = 300,
     }
 }
