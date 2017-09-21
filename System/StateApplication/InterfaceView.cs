09a2a5a4a65ce48c8c561d17ef905dbda7c7114c
Vorlias <void.knight09@gmail.com>
2017-09-22 09:03:02 +1200

    InterfaceView, InterfaceViewSingleton<T>

---
+++ System/StateApplication/InterfaceView.cs
@@ -0,0 +1,34 @@
+﻿using Andromeda2D.Entities;
+using Andromeda2D.Entities.Components;
+using Andromeda2D.System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Andromeda.System
+{
+    /// <summary>
+    /// A GameView which has a UserInterface entity by default
+    /// </summary>
+    public abstract class InterfaceView : GameView
+    {
+        UserInterface ui;
+        public UserInterface UI
+        {
+            get => ui;
+        }
+
+        public abstract void OnInterfaceStart(UserInterface ui);
+
+        public override void OnStart()
+        {
+            Entity uiEntity = new Entity(this);
+            ui = uiEntity.AddComponent<UserInterface>();
+            uiEntity.Name = "Interface";
+
+            OnInterfaceStart(ui);
+        }
+    }
+}
---
+++ System/StateApplication/InterfaceViewSingleton.cs
@@ -0,0 +1,57 @@
+﻿using Andromeda.System;
+using Andromeda2D.Entities.Components;
+using System;
+
+namespace Andromeda2D.System
+{
+    public abstract class InterfaceViewSingleton<InterfaceViewClass> : InterfaceView, IGameViewSingleton where InterfaceViewClass : InterfaceView, new()
+    {
+        static InterfaceViewClass singleton;
+
+        static InterfaceViewSingleton()
+        {
+        }
+
+        static bool started = false;
+
+        /// <summary>
+        /// Similar to OnStart() for regular GameViews, but called when the Singleton is initialized
+        /// </summary>
+        public abstract void OnUISingletonStart(UserInterface ui);
+
+
+        public sealed override void OnAdded()
+        {
+            // Do nothing because it doesn't get added
+        }
+
+        public sealed override void OnInterfaceStart(UserInterface ui)
+        {
+            if (!started)
+            {
+                started = true;
+                OnUISingletonStart(ui);
+            }
+        }
+
+        public new bool IsSingleton()
+        {
+            return true;
+        }
+
+        public static InterfaceViewClass Instance
+        {
+            get
+            {
+                if (singleton == null)
+                {
+                    Console.WriteLine("Created singleton (.Instance)");
+                    singleton = new InterfaceViewClass();
+                    // singleton.OnStart();
+                }
+
+                return singleton;
+            }
+        }
+    }
+}
