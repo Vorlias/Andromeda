a7d5eabd6928386805a4186c065fc23231865984
Vorlias <void.knight09@gmail.com>
2017-09-25 11:07:12 +1300

    EngineInfo updated

--- System/EngineInfo.cs
+++ System/EngineInfo.cs
@@ -8,9 +8,9 @@ namespace Andromeda2D.System
 {
     public static class EngineInfo
     {
-        const int DATE = 170828;
+        const int DATE = 170925;
         const int MAJOR = 0;
-        const int MINOR = 300;
+        const int MINOR = 360;
         const int REVISION = 0;
 
         const int FULL = (MAJOR * 10000) + (MINOR * 100) + REVISION;
