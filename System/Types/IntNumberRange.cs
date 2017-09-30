df4bd2cef411159d6974e314a692ac5db42a866c
Vorlias <void.knight09@gmail.com>
2017-10-01 00:32:56 +1300

    Added IntNumberRange

---
+++ System/Types/IntNumberRange.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Andromeda2D.System.Types
+{
+    public struct IntNumberRange : INumberRange<int>
+    {
+        static Random numberRangeRandom = new Random();
+
+        public IntNumberRange(int value)
+        {
+            Min = value;
+            Max = value;
+        }
+
+        public IntNumberRange(int min, int max)
+        {
+            Min = min;
+            Max = max;
+        }
+
+        public int Min { get; set; }
+        public int Max { get; set; }
+
+        public int Random => numberRangeRandom.Next(Min, Max);
+
+        public int Clamped(int value)
+        {
+            if (value > Max)
+                return Max;
+            else if (value < Min)
+                return Min;
+            else
+                return value;
+        }
+
+        public bool InRange(int value)
+        {
+            return (value >= Min) && (value <= Max);
+        }
+
+        public bool Overlaps(INumberRange<int> other)
+        {
+            return !(other.Max < Min || other.Min > Max);
+        }
+    }
+}
