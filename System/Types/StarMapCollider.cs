4c9cd1cbd2ff8ef719c8ac8d1e78e80bc77f021d
Jonathan Holmes <Vorlias@users.noreply.github.com>
2017-10-05 18:41:55 +1300

    Add StarMapCollider

---
+++ System/Types/StarMapCollider.cs
@@ -0,0 +1,32 @@
+﻿using Andromeda2D.System.Types;
+using SFML.System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Andromeda.Colliders
+{
+    /// <summary>
+    /// A collider that works with the Star Map editor for Andromeda
+    /// </summary>
+    public class StarMapCollider
+    {
+        public Vector2u Size
+        {
+            get;
+        }
+
+        public Polygon Polygon
+        {
+            get;
+        }
+
+        public StarMapCollider(Polygon polygon, Vector2u size)
+        {
+            Size = size;
+            Polygon = polygon;
+        }
+    }
+}
