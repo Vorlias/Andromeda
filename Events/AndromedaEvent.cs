7a8c6c5d1fdaabd431b47edc2fc74c36302d4f3e
Jonathan Holmes <Vorlias@users.noreply.github.com>
2017-11-07 00:31:52 +1300

    Added AndromedaEvent
    New event handling system for Andromeda using delegates.

---
+++ Events/AndromedaEvent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Andromeda2D.Events
+{
+    public struct AndromedaEvent
+    {
+        public event Action Event;
+
+        public void Invoke() => Event?.Invoke();
+
+        public static AndromedaEvent operator+(AndromedaEvent lhs, Action rhs)
+        {
+            lhs.Event += rhs;
+            return lhs;
+        }
+    }
+
+    public struct AndromedaEvent<T>
+    {
+        public event Action<T> Event;
+
+        public void Invoke(T value) => Event?.Invoke(value);
+
+        public static AndromedaEvent<T> operator +(AndromedaEvent<T> lhs, Action<T> rhs)
+        {
+            lhs.Event += rhs;
+            return lhs;
+        }
+    }
+}
