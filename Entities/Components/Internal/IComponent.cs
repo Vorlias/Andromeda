3762856a10ed76aa6ae8af215e88808d74ac1833
Jonathan Holmes <Vorlias@users.noreply.github.com>
2017-12-14 10:11:00 +1000

    Added IComponent.TryCast<T>
    Used for less ugly casting for derived component types

--- Entities/Components/Internal/IComponent.cs
+++ Entities/Components/Internal/IComponent.cs
@@ -5,6 +5,37 @@ using System.Threading.Tasks;
 
 namespace Andromeda2D.Entities.Components
 {
+    public static class ComponentExtension
+    {
+        /// <summary>
+        /// Used to turn abstract components into derived types
+        /// </summary>
+        /// <typeparam name="T">The type of the component</typeparam>
+        /// <param name="self"></param>
+        /// <param name="component">The destination component object</param>
+        /// <returns>True if derived type is valid for the component</returns>
+        public static bool TryCast<T>(this IComponent self, out T component) where T : IComponent
+        {
+            // The purpose of this function is to save me doing
+            // if (component is DerivedComponent) 
+            //      (component as DerivedComponent).DoAction();
+            //
+            // Instead, can do this - which is more readable
+            // if (component.TryCast(out DerivedComponent derived))
+            //      derived.DoAction();
+
+            if (self is T)
+            {
+                component = (T)self;
+                return true;
+            }
+            else
+            {
+                component = default(T);
+                return false;
+            }
+        }
+    }
 
     public interface IComponent
     {
