c6a7c812f45d2847887d8794a17f80cd4de48508
Vorlias <void.knight09@gmail.com>
2018-07-14 18:23:59 +1200

    Fixed GetAspectRatio( )

--- System/Utility/ScreenUtil.cs
+++ System/Utility/ScreenUtil.cs
@@ -18,7 +18,7 @@ namespace Andromeda.System.Utility
         /// </summary>
         /// <param name="mode">The video mode</param>
         /// <returns>The aspect ratio (Height, Width)</returns>
-        public Vector2u GetAspectRatio(VideoMode mode)
+        public static Vector2u GetAspectRatio(VideoMode mode)
         {
             uint nGCD = GetGreatestCommonDivisor(mode.Height, mode.Width);
             return new Vector2u(mode.Height / nGCD, mode.Width / nGCD);
