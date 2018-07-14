dc01d4ac4d78b485dad1aff20c76febdb6c19c11
Vorlias <void.knight09@gmail.com>
2018-07-14 18:21:25 +1200

    Add ScreenUtil

---
+++ System/Utility/ScreenUtil.cs
@@ -0,0 +1,32 @@
+﻿using SFML.System;
+using SFML.Window;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Andromeda.System.Utility
+{
+    /// <summary>
+    /// Screen-based utilities
+    /// </summary>
+    public class ScreenUtil
+    {
+        /// <summary>
+        /// Get the aspect ratio of the specified video mode
+        /// </summary>
+        /// <param name="mode">The video mode</param>
+        /// <returns>The aspect ratio (Height, Width)</returns>
+        public Vector2u GetAspectRatio(VideoMode mode)
+        {
+            uint nGCD = GetGreatestCommonDivisor(mode.Height, mode.Width);
+            return new Vector2u(mode.Height / nGCD, mode.Width / nGCD);
+        }
+
+        private static uint GetGreatestCommonDivisor(uint a, uint b)
+        {
+            return b == 0 ? a : GetGreatestCommonDivisor(b, a % b);
+        }
+    }
+}
