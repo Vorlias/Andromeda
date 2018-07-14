6ae3cd87c0d92e1e5674332530107e627278cd6b
Vorlias <void.knight09@gmail.com>
2018-07-14 18:16:29 +1200

    StarMapCollider constructor change
    Now takes Stream

--- System/Types/StarMapColliderInfo.cs
+++ System/Types/StarMapColliderInfo.cs
@@ -1,7 +1,9 @@
 ﻿using Andromeda.System.Types;
+using Andromeda.System.Utility;
 using SFML.System;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,8 +25,35 @@ namespace Andromeda.Colliders
             get;
         }
 
-        public StarMapColliderInfo(Polygon polygon, Vector2u size)
+        /// <summary>
+        /// Read StarMap collider data from a stream
+        /// </summary>
+        /// <param name="stream"></param>
+        public StarMapColliderInfo(Stream stream)
         {
+            Polygon polygon = new Polygon();
+
+            int version, vertexCount;
+            Vector2u size;
+            bool autoSize;
+
+            using (stream)
+            {
+                version = stream.ReadByte();
+                autoSize = stream.ReadBool();
+                size = stream.ReadVector2u();
+
+                vertexCount = stream.ReadInt32();
+
+                for (int i = 0; i < vertexCount; i++)
+                {
+                    Vector2i vertex = stream.ReadVector2i();
+                    polygon.Add(vertex.ToFloat());
+                }
+            }
+
+            stream.Close();
+
             Size = size;
             Polygon = polygon;
         }
